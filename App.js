d6f342223132097557aaed89c490a5b59f12f1e3
Kim Ruedas <kruedas09@alumnos.uaq.mx>
2023-09-24 21:30:45 -0600

    🐕 feat(ui): revamp app layout and add header with logo  🐕

--- App.js
+++ App.js
@@ -1,41 +1,46 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import * as ImagePicker from "expo-image-picker";
 
 import Button from "./components/Button";
 import ImageViewer from './components/ImageViewer';
 
-const PlaceholderImage = require('./assets/images/background-image.png');
+const PlaceholderImage = require('./assets/images/based.png');
+const LogoImage = require('./assets/images/logo.png'); // Make sure the image is placed at this path
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const pickImageAsync = async() => {
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-        allowsEditing: true,
-        quality: 1,
+      allowsEditing: true,
+      quality: 1,
     });
-  
-    if (!result.canceled){
+
+    if (!result.canceled) {
       setSelectedImage(result.assets[0].uri)
       console.log(result);
     } else {
-      alert('You did not select any image.');
+      alert('No se selecciono ninguna imagen');
     }
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={LogoImage} style={styles.logo} />
+        <Text style={styles.title}>OnlyDogs</Text>
+      </View>
       <View style={styles.imageContainer}>
-        <ImageViewer 
-          placeholderImageSource={PlaceholderImage} 
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="use this photo" />
+        <Button label="Usar esta imagen" />
+        <Button theme="primary" label="Elegir una imagen" onPress={pickImageAsync} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -45,17 +50,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 60,
+  },
+  logo: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
   imageContainer: {
     flex: 1,
     paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
   footerContainer: {
     flex: 1 / 3,
